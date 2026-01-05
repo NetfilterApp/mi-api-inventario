d9bc7c52dbfcd70817311822661f93a4aecf06c1
NetfilterApp <antonio.barragana1970@gmail.com>
2026-01-05 13:08:58 -0400

    app.js

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
-const mysql = require('mysql2');
-
-// Configuración de la conexión
 const conexion = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', // En Termux root no suele tener clave
-  database: 'mi_app'
-});
-
-// Realizar la consulta a tu tabla 'inventario'
-conexion.query('SELECT * FROM inventario', (err, filas) => {
-  if (err) {
-    console.error('Error en la consulta: ', err);
-    return;
-  }
-  console.log('--- PRODUCTOS EN TU APP ---');
-  console.table(filas);
-  conexion.end();
+  host: 'mysql.railway.internal', // O el "TCP Proxy" que aparece en tus credenciales
+  user: 'root', 
+  password: 'dXSbAYDTFlNYvywtSXhekoAoinHejZJM', 
+  database: 'railway', 
+  port: 3306 
 });
